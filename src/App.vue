1d99c3417e128dd71c40ecb74fbbb9ffcfcc56ab
Anton <anton.gusevv.01@gmail.com>
2023-08-04 23:38:09 +0300

    feat: добавил шрифт

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app"></div>
+  <div id="app">Тестирование шрифта Test of fonts</div>
 </template>
 
 <script>
@@ -7,14 +7,3 @@ export default {
   name: 'App',
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
